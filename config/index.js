ae729e72c3f8fe2409a49f28ae2198f5da95a4d5
Aaron Ferrer <aaronferrerbarbas@gmail.com>
2025-01-14 15:40:10 +0100

    update

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ const logger = require("morgan")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
 
-const FRONTEND_URL = process.env.ORIGIN || 'https://solentine.netlify.app/'
+const FRONTEND_URL = process.env.ORIGIN || 'https://solentine.netlify.app'
 
 module.exports = (app) => {
   app.set("trust proxy", 1)
