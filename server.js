c710f6a6c518e483df4cbbb74de8894fe547b865
Aaron Ferrer <aaronferrerbarbas@gmail.com>
2025-01-16 15:38:34 +0100

    change in server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require("./app")
 
 const PORT = process.env.PORT || 5005
+const HOST = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost'
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`)
+  console.log(`Server listening on http://${HOST}:${PORT}`)
 })
