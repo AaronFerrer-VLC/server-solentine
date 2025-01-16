2ae50ff8629f58abe7dc6299d40b35c03f420b4e
Aaron Ferrer <aaronferrerbarbas@gmail.com>
2025-01-16 17:30:09 +0100

    fixed app.js

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ require("dotenv").config();
 require("./db");
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const path = require("path");
+
+const allowedOrigins = ['http://localhost:5173', 'https://solentine.netlify.app'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
