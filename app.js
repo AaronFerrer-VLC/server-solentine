e2dd81ae90fcd55f00b99f6afd9d7f446cdb180b
Aaron Ferrer <aaronferrerbarbas@gmail.com>
2025-01-19 18:43:53 +0100

    add new update

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-require("dotenv").config()
+require("dotenv").config({
+    path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env'
+})
+
 require("./db")
 
 const express = require("express")
